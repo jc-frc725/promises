020e069dc7e3ad071a32195e6e25cbf1ac618157
Justin Chow <hrll71990@gmail.com>
2020-12-17 00:14:24 -0800

    bare minimums complete

--- exercises/bare_minimum/basicChaining.js
+++ exercises/bare_minimum/basicChaining.js
@@ -10,11 +10,17 @@
 
 var fs = require('fs');
 var Promise = require('bluebird');
-
-
+var makeReq = require('./promisification.js');
+var getUser = require('./promiseConstructor.js');
+var writeFile = Promise.promisify(fs.writeFile);
 
 var fetchProfileAndWriteToFile = function(readFilePath, writeFilePath) {
   // TODO
+  return getUser.pluckFirstLineFromFileAsync(readFilePath)
+    .then(makeReq.getGitHubProfileAsync)
+    .then((user) => {
+      return writeFile(writeFilePath, JSON.stringify(user));
+    });
 };
 
 // Export these functions so we can test them
